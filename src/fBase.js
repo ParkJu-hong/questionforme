b691fa119a2bd082ac6aad64f916b3a61f40b4c1
bejeju <bejejupark@gmail.com>
2022-01-23 16:07:15 +0900

    plz done

--- src/fBase.js
+++ src/fBase.js
@@ -3,7 +3,15 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
-
+/*
+REACT_APP_API_KEY=AIzaSyCZe2abS_Lq_lEELjVQj0pakeTTOzQtXFk
+REACT_APP_AUTH_DOMAIN=nomadcoding-d8c5f.firebaseapp.com
+REACT_APP_PROJECT_ID=nomadcoding-d8c5f
+REACT_APP_STORAGE_BUCKET=nomadcoding-d8c5f.appspot.com
+REACT_APP_MESSAGIN_ID=21189401175
+REACT_APP_APP_ID=1:21189401175:web:37f77bf0fc5abb6cba6112
+ */
+/*
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,6 +19,15 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
     appId: process.env.REACT_APP_APP_ID
+};
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyCZe2abS_Lq_lEELjVQj0pakeTTOzQtXFk",
+    authDomain: "nomadcoding-d8c5f.firebaseapp.com",
+    projectId: "nomadcoding-d8c5f",
+    storageBucket: "nomadcoding-d8c5f.appspot.com",
+    messagingSenderId: "21189401175",
+    appId: "1:21189401175:web:37f77bf0fc5abb6cba6112"
 };
 
 firebase.initializeApp(firebaseConfig);
